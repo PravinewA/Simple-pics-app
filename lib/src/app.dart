92c55ce7fcfeea2b92c2f31ae9630b19f540a194
Pravinewa <whopravinewa@gmail.com>
2020-01-23 17:06:13 +0545

    Simple flutter app

---
+++ lib/src/app.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'dart:convert';
+import 'models/image_model.dart';
+import 'widgets/image_list.dart';
+
+class App extends StatefulWidget{
+  createState(){
+    return AppState();
+  }
+}
+
+class AppState extends State<App>{
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async{
+    counter++;
+    var response = await get('http://jsonplaceholder.typicode.com/photos/$counter');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
+  Widget build(context){
+    return MaterialApp(
+      home : Scaffold(
+        body: ImageList(images),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            setState(fetchImage);
+        },
+        child: Icon(Icons.add),),
+        appBar : AppBar(
+          title: Text('Let see some Images!!'),
+        ),
+      ),
+    );
+  }
+}
